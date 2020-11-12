16b17e5282827ebdd4929e248ab32e06c4f25c45
AM Shovon <shovonbd321@gmail.com>
2020-11-12 12:08:06 +0600

    update

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.post("/", function(req, res) {
     const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+ apiKey +"&units="+ unit;
     https.get(url, function(response) {
         console.log(response.statusCode)
+        // Weather website
 
         response.on("data", function(data){
             const weatherData = JSON.parse(data)
